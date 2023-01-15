0323a408b513b8723084a204ce45adf31fc4e575
munashex <munashemugondaa@gmail.com>
2023-01-16 00:15:01 +0200

    feature-a

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom"
 
+//this a react web
+
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
